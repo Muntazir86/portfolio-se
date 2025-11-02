2436911d7a9ea59f27d50d5fce424a6c75e5dc7c
Ghulam Shabbir <ghulamshabbirmtn500@gmail.com>
2025-11-03 00:11:28 +0500

    Updates

---
+++ _next/static/chunks/512fa288277663cd.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/5c751f2ab501672f.js",
+  "static/chunks/turbopack-fb9ab4652ec6bae0.js"
+])
---
+++ _next/static/chunks/7416d33d574945f7.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/5c751f2ab501672f.js",
+  "static/chunks/turbopack-54d31c88d186c4b7.js"
+])
